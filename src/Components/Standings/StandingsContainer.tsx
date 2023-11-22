3645dacd3704f93a998f4bf56dabde5aca5fabf7
debortolin <57450369+nick-debortoli@users.noreply.github.com>
2023-11-21 21:09:16 -0500

    Add tabs for each office in standings

--- src/Components/Standings/StandingsContainer.tsx
+++ src/Components/Standings/StandingsContainer.tsx
@@ -1,17 +1,24 @@
 import { useEffect, useState } from 'react';
 import StandingsRow from './StandingsRow';
 import StandingsHeading from './StandingsHeading';
-import { Standing } from '../../Types/dataTypes';
+import { Office, Standing } from '../../Types/dataTypes';
 import './StandingsContainer.scss';
 import { usePlayers } from '../../Contexts/PlayersContext';
+import OfficeTabs from './OfficeTabs';
 
 const StandingsContainer: React.FC = () => {
     const [standingsData, setStandingsData] = useState<Standing[] | null>(null);
+    const [focusedOffice, setFocusedOffice] = useState<Office>(Office.PGH);
     const { players } = usePlayers();
 
     useEffect(() => {
         const fetchData = async () => {
-            const sortedPlayers = players
+            const filteredPlayers =
+                focusedOffice !== Office.InterOffice
+                    ? players?.filter((player) => player.office === focusedOffice)
+                    : players;
+
+            const sortedPlayers = filteredPlayers
                 .slice()
                 .sort((playerA, playerB) => playerB.elo - playerA.elo);
 
@@ -29,10 +36,15 @@ const StandingsContainer: React.FC = () => {
         };
 
         fetchData();
-    }, [players]);
+    }, [players, focusedOffice]);
+
+    const handleOfficeSelect = (office: Office): void => {
+        setFocusedOffice(office);
+    };
 
     return (
         <div className="standings-container">
+            <OfficeTabs focusedOffice={focusedOffice} handleSelect={handleOfficeSelect} />
             <StandingsHeading />
             {standingsData &&
                 standingsData.map((standing, index) => (
