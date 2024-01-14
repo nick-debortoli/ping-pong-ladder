d0408c3288d6fcc137f3a8d3065dfbbb09e062f8
debortolin <57450369+nick-debortoli@users.noreply.github.com>
2024-01-14 17:36:57 -0500

    Add rules

--- src/Components/Tournaments/TournamentsContainer.tsx
+++ src/Components/Tournaments/TournamentsContainer.tsx
@@ -5,6 +5,8 @@ import OfficeToggle from '../OfficeToggle/OfficeToggle';
 import './TournamentsContainer.scss';
 import { getTournamentLogo } from '../../Utils/tournamentUtils';
 import { useTournaments } from '../../Contexts/TournamentContext';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import { Tooltip } from '@mui/material';
 
 const TournamentsContainer: React.FC = () => {
     const [activeTournament, setActiveTournament] = useState<Tournament | null>(null);
@@ -35,6 +37,41 @@ const TournamentsContainer: React.FC = () => {
         setActiveOffice(office);
     };
 
+    const rules = (
+        <>
+            <div>Rules</div>
+            <br />
+            <div>General</div>
+            <div>
+                The tournaments are to be played in each office independently. There will be a
+                winner crowned for each office. Regular season games can still be played during the
+                tournament, but they will not count as tournament matches
+            </div>
+            <br />
+            <div>Round Play</div>
+            <div>
+                A round is played best 2 out of 3 matches to 21, win by 2. The exception to this is
+                for the Semifinals and Finals; these we be played as best 3 out of 5. Games in a
+                round do not have to be played at the same time - they can be played one after the
+                other or on multiple days.
+            </div>
+            <br />
+            <div>Dates</div>
+            <div>
+                The tournament starts on Monday, January 15 and ends on Friday, February 2. All play
+                must be finished by this date.
+            </div>
+            <br />
+            <div>Forfeits</div>
+            <div>
+                All matches must be played within one week of being challenged by your opponent. If
+                this does not happen, the challenger automatically advances and their opponent
+                forfeits. Additionally, players can opt to forfeit on their own if they do not feel
+                that they can complete the matches.
+            </div>
+        </>
+    );
+
     return (
         <div className="tournaments-container">
             {activeTournament && (
@@ -49,6 +86,9 @@ const TournamentsContainer: React.FC = () => {
                                 />
                             )}
                             {activeTournament.name}
+                            <Tooltip title={rules}>
+                                <InfoOutlinedIcon sx={{ cursor: 'pointers' }} />
+                            </Tooltip>
                         </h3>
                         <OfficeToggle onOfficeClick={handleOfficeClick} hasBorder={true} />
                     </div>
