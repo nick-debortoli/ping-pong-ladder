5796cedf3f6b849926ae3241a261135aa80d9454
debortolin <57450369+nick-debortoli@users.noreply.github.com>
2024-02-15 17:52:45 -0500

    Remove player migration

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -2,10 +2,8 @@ import MainContent from '../MainContent/MainContent';
 import Sidebar from '../Sidebar/Sidebar';
 import './Home.scss';
 import SettingsMenu from '../SettingsMenu/SettingsMenu';
-import { migratePlayers } from '../../database/firestore';
 
 const Home: React.FC = () => {
-    migratePlayers();
     return (
         <div className="home">
             <SettingsMenu />
